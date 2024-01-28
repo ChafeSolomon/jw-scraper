4b007621df56989ead4baf2f994268742a22073a
Chafe Solomon <chafe.solomon@tresata.com>
2024-01-28 16:50:45 -0500

    Optimized code

--- src/main.py
+++ src/main.py
@@ -4,13 +4,18 @@ from bs4 import BeautifulSoup as soup
 URL = "https://wol.jw.org/en/wol/h/r1/lp-e"
 page = requests.get(URL)
 
+id = ["p77", "p78", "p79"]
 soup = soup(page.content, "html.parser")
-results_date = soup.find(id="p77")
-results_text = soup.find(id="p78")
-results_text1 = soup.find(id="p79")
-results_date = results_date.get_text()
-results_text = results_text.get_text()
-results_text1 = results_text1.get_text()
-print(str(results_date).strip())
-print(str(results_text).strip() + "\n")
-print(str(results_text1).strip())
+
+def printtext(pid):
+    sep = 0
+    for i in pid:
+        sep += 1
+        results = soup.find(id=i).get_text()
+        if sep == 2:
+            print(str(results).strip() + "\n")
+
+        else: 
+            print(str(results).strip())
+
+printtext(id)
